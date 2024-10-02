873c7f6ec8cd3e9dbcac35b11862f0337ff0d3c1
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-02 15:44:04 +0700

    add port server

--- server/index.js
+++ server/index.js
@@ -6,3 +6,12 @@ dotenv.config()
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(cookiePaeser())
+app.use(cors())
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
