41a24cf82477734e2d7b5b025faf6c32d9485349
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-03 15:48:01 +0700

    add improt prisma client

--- server/config/prismaConfig.js
+++ server/config/prismaConfig.js
@@ -1,4 +1,3 @@
-import { PrismaClient, prismaClient } from '@prisma/client';
-const prisma = new PrismaClient()
-
-export {prisma}
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient(); 
+export { prisma };
