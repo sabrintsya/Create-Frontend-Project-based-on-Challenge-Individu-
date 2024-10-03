80d4d84f64c9a70c2753ac81731c8fbbd81e6899
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-03 15:46:47 +0700

    memperbaiki improt route

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv'; 
 import cookiePaeser  from "cookie-parser";
 import cors from 'cors';
+import { userRoute } from './routes/userRoute.js';
 dotenv.config()
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json())
 app.use(cookiePaeser())
@@ -15,3 +16,5 @@ app.use(cors())
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.use('/api/user', userRoute)
