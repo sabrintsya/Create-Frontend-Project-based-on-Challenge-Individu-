cbab7ca21d5c1ef56d0ed5cc1a7e6dc309c79abb
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-03 19:56:03 +0700

    add api for course

--- server/controllers/courseCntrl.js
+++ server/controllers/courseCntrl.js
@@ -3,5 +3,42 @@ import asyncHandler from "express-async-handler";
 import { prisma } from '../config/prismaConfig.js'
 
 export const createCourse = asyncHandler(async (req, res) => {
-    console.log("endpoint created")
+   const {
+      title, 
+      description, 
+      price, 
+      address, 
+      city, 
+      country, 
+      image, 
+      facilities, 
+      userEmail, 
+   } = req.body.data
+
+   console.log(req.body.data)
+   try {
+
+      const course = await prisma.course.create({
+         data: {
+            title,  
+            description, 
+            price, 
+            address, 
+            country, 
+            city, 
+            facilities, 
+            image, 
+            owner : {connect : {email: userEmail} },
+         },
+      });
+
+      res.send({})
+
+   } catch(err) {
+      if(err.code === "P2002")
+      {
+         throw new Error("A course with address alredy there")
+      }
+      throw new Error("err.message")
+   }
 })
